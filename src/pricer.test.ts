0483da9fe1718489f7727aab3e87631a206a5f0e
avramsternGL <avram.stern@gorillalogic.com>
2026-02-09 15:04:00 -0600

    test: add additional coverage for pricer

--- src/pricer.test.ts
+++ src/pricer.test.ts
@@ -16,3 +16,34 @@ it('provides the latest price given the options selected so far', () => {
   const priceAfterThirdSelection = pricer('size', 'large');
   expect(priceAfterThirdSelection).toBe(2.5);
 });
+
+it('replaces previous selection instead of accumulating', () => {
+  const pricer = createPricer();
+  pricer('size', 'large');
+  pricer('size', 'medium');
+
+  expect(pricer('size', 'small')).toBe(1.0);
+});
+
+it('prices for each size correctly with NO creamer', () => {
+  const pricer = createPricer();
+  pricer('creamer', 'none');
+
+  pricer('size', 'small');
+  expect(pricer('size', 'small')).toBe(1.0);
+
+  expect(pricer('size', 'medium')).toBe(1.5);
+
+  expect(pricer('size', 'large')).toBe(2.0);
+});
+
+it('prices each creamer option correctly', () => {
+  const pricer = createPricer();
+  pricer('size', 'medium');
+
+  expect(pricer('creamer', 'none')).toBe(1.5);
+
+  expect(pricer('creamer', 'dairy')).toBe(1.75);
+
+  expect(pricer('creamer', 'non-dairy')).toBe(2.0);
+});
